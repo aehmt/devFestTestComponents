45ecdd9cab5c366a0830889559d8e2c684ac2346
Margaret <eutopia@gmail.com>
2016-11-19 15:23:54 -0500

    fixed merge conflicts

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react'
 import Events from './events'
 import FilteredUsers from './filteredUsers'
 import React, { Component } from 'react';
@@ -19,22 +17,6 @@ export default class App extends React.Component{
     this.eventSearch('karaoke')
   }
 
-  render(){
-    if (this.state.index){
-      return (
-        <div id="eventContainer">
-          <p>Events for you to Check out:</p>
-          <Events events={this.state.events}/>
-        </div>
-      )
-    } else {
-      return (
-        <div id = "userMenu">
-          <FilteredUsers />
-        </div>
-      )
-    }
-
   eventSearch(term) {
     const OAuth_token = 'FEELRVTAKH4YPZX4BKQT';
     var url = `https://www.eventbriteapi.com/v3/events/search/?q=${term}&token=${OAuth_token}`
@@ -51,11 +33,20 @@ export default class App extends React.Component{
 
   render(){
     const eventSearch = _.debounce((term) => {this.eventSearch(term)}, 500);
-    return (
-      <div>
-        <SearchBar onSearchTermChange={eventSearch} />  
-        <EventList events={this.state.events} />
-      </div>
-    )
+    if (this.state.index){
+      return (
+        <div id="eventContainer">
+          <p>Events for you to check out:</p>
+          <SearchBar onSearchTermChange={eventSearch} />  
+          <EventList events={this.state.events} />
+        </div>
+      )
+    } else {
+      return (
+        <div id = "userMenu">
+          <FilteredUsers />
+        </div>
+      )
+    }
   }
 }
