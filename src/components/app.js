a873700d61f1dd1808a33837de4245e6603ce22d
satub <satu.barnhill@gmail.com>
2016-11-19 16:15:36 -0500

    bugFix

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,4 @@
-import Events from './events'
+// import Events from './events'
 import FilteredUsers from './filteredUsers'
 import React, { Component } from 'react';
 import SearchBar from './search_bar.js';
