3bd62b403931e3d8648680767cb119266a4547ea
satub <satu.barnhill@gmail.com>
2016-11-19 19:48:32 -0500

    user and events on the same page

--- src/components/event_list.js
+++ src/components/event_list.js
@@ -2,14 +2,18 @@ import React from 'react';
 import EventListItem from './event_list_item'
 import EventDetail from './event_detail'
 import InterestedButton from './interestedButton'
+import FilteredUsers from './filteredUsers'
+
 export default class EventList extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       showDetails: false,
-      zoomTo: null
+      zoomTo: null,
+      userDiv: false
     }
     this.showDetail = this.showDetail.bind(this)
+    this.showUsers = this.showUsers.bind(this)
   }
 
   showDetail(ev){
@@ -20,16 +24,25 @@ export default class EventList extends React.Component {
     })
   }
 
+  showUsers(ev){
+    this.setState({
+      userDiv: true
+    })
+  }
+
   render(){
     return (
       <div>
         <div>
           <ul>
-            {this.props.events.map((event) => <div><EventListItem handleChoice={this.showDetail} event={event} key={event.id} /><InterestedButton /></div>)}
+            {this.props.events.map((event) => <div><EventListItem handleChoice={this.showDetail} event={event} key={event.id} /><InterestedButton handleUsers={this.showUsers}/></div>)}
             </ul>
         </div>
         <div>
           {this.state.showDetails ? <EventDetail stuff = {this.state.zoomTo}/> : ''}
+        </div>
+        <div>
+          {this.state.userDiv ? <FilteredUsers going={this.props.interestedUsers} filter={this.props.filterBy}/>: ''}
         </div>
       </div>
     )
--- src/components/interestedButton.js
+++ src/components/interestedButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function InterestedButton(props){
   return (
-    <button>
+    <button onClick={props.handleUsers}>
       Interested? (But I do not work yet, sorry!)
     </button>
   )
