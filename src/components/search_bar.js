66e1000dc353a8a587b621e4c7df5deae31b7bb6
Gordon Gao <gordonsdev@gmail.com>
2016-11-20 10:48:35 -0500

    fix search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -23,7 +23,7 @@ class SearchBar extends Component {
             <a href="#"><img className="search-icon" src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png"/></a>
             </div>
             <div className="col-md-8">
-            Event:<input type="text" id="search-bar" placeholder="What can I help you with today?" value={this.state.term} onChange={event => this.onInputChange(event.target.value, this.state.location)} />
+            <input type="text" id="search-bar" placeholder="What can I help you with today?" value={this.state.term} onChange={event => this.onInputChange(event.target.value, this.state.location)} />
             Location:<input type="text" id="location-search-bar" value={this.state.location} onChange={event => this.onInputChange(this.state.term, event.target.value)} />
             </div>
           </div>
