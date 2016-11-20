d785f3ffaa455f0939609bf6d5b3bbfa8d5f90db
satub <satu.barnhill@gmail.com>
2016-11-20 11:31:54 -0500

    removed unneccessary comments

--- src/components/interestedButton.js
+++ src/components/interestedButton.js
@@ -1,22 +1,11 @@
 import React from 'react'
 import { pushData } from './firebase/firebase';
 
-
-// var config = {
-//     apiKey: "AIzaSyB9jSHM0DXvi-dX0hJ0P675ndDbzqp8ZP0",
-//     authDomain: "thoughtdot-e75ba.firebaseapp.com",
-//     databaseURL: "https://thoughtdot-e75ba.firebaseio.com",
-//     storageBucket: "thoughtdot-e75ba.appspot.com",
-//     messagingSenderId: "396643647275"
-// };
-// firebase.initializeApp(config);
-
 export default function InterestedButton(props){
   let thisUserWantsToGo = props.assignEmail;
   let thisEvent = props.assignEvent;
   function sendStuffToDB(){
     console.log(thisUserWantsToGo + " going to event # " + thisEvent)
-    // console.log(firebase)
 
     let filters = ["friends", "date", "group"]
     let rndPurpose = filters[Math.floor(Math.random()*3)];
