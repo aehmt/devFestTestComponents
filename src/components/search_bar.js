86500ddf68e1e9a912ecf961a1a0a60be9db431b
Gordon Gao <gordonsdev@gmail.com>
2016-11-20 10:46:22 -0500

    style search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -18,8 +18,14 @@ class SearchBar extends Component {
     return (
       <div>
         <form className="search-container">
-          <a href="#"><img className="search-icon" src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png"/></a>
-          <input type="text" id="search-bar" placeholder="What can I help you with today?"/>
+          <div className="row">
+          <div className="col-md-2">
+            <a href="#"><img className="search-icon" src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png"/></a>
+            </div>
+            <div className="col-md-8">
+            <input type="text" id="search-bar" placeholder="What can I help you with today?"/>
+            </div>
+          </div>
         </form>
       </div>
     );
